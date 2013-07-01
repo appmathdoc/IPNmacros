a0b05461ab7d2d60d11a7a0118fc9c5efbeba48e
Jeff Knisley <knisleyj@etsu.edu>
2013-07-01 10:28:29 -0400

    Update Detachables

--- DetachableViews.py
+++ DetachableViews.py
@@ -1,44 +1,49 @@
-## Firefox, Chrome,  
+## Doesn't yet work in Firefox for every possible argument (waiting for srcdoc -- Probably Aug 2013 in Firefox 23) 
+
+## Tested on Chrome 28, Firefox 22, and Safari 5.1.7 on Windows 7 Home, Professional
 
 from IPython.display import HTML
 
 class DetachableView:
-    
+    "ONLY DOUBLE QUOTES PERMITTED IN HTMLview -- use &apos; for single quotes"
     def __init__(my, PostType = "OneWay" ): #PostType is for later
         my.framecntr = 0 
-        #HTML( """  """)
-                 
-    def HTMLView( my, HTMLview, toggle = True, width=None, height=None):
+
+    def URLView( my, URL, toggle = True, width=None, height=None):
+        """ URLView(URL) -> Detachable View of the page at URL
+        
+            ONLY DOUBLE QUOTES PERMITTED IN HTMLview -- use &apos; for single quotes"""
+        
+        # Each LaunchView is unique -- probably could have one LaunchView + arguments, 
+        # but the data to be displayed (i.e., the would-be argument) is the lion's 
+        # share of the definition
         my.framecntr += 1
-        HtmlString  = ""
+        
+        HtmlString = ""
+        # toggle = False suppresses the popup window
         if( toggle):
             HtmlString += """
 
-                    <script type="text/javascript"> 
+           <script type='text/javascript'> 
                 
                 function LaunchView%s( ) {
                     var iFrame = document.getElementById('IframeView%s');
-                    var iFcont = "%s"
-                    var DetachedName = "Detached%s";
-                    if( iFrame.style.display === "block") {
-                        var win = window.open('about:blank', DetachedName ,'height=500,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+                    if( iFrame.style.display == 'block') {
+                        var win = window.open( '%s', 'Detached%s' ,'height=500,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
                         
-                        var win_doc = win.document;
-                        win_doc.open();
-                        win_doc.write("<!DOCTYPE html><htm" + "l><head><body>"+ iFcont +"</body></ht" + "ml>");
-                        win_doc.close();
-                        iFrame.style.display = "none"
+                        iFrame.style.display = 'none'
                     } else { 
-                        iFrame.style.display = "block"
+                        iFrame.style.display = 'block'
                     };
                 }
                 
             </script>  
       
-                <input type="button" value="toggle view" onclick="LaunchView%s()"> <br>
+                <input type='button' value='toggle view' onclick='LaunchView%s()'> <br>
+                
+            """ % ( my.framecntr, my.framecntr, URL, my.framecntr, my.framecntr ) 
         
-            """ % ( my.framecntr, my.framecntr, HTMLview, my.framecntr,my.framecntr ) 
-        
+        # Set width, height
         if( width == None):
             width = "95%"
         elif( type(width) != type( "500px" )):
@@ -48,14 +53,93 @@ class DetachableView:
             height = "24em"
         elif( type(height) != type( "500px" )):
             height = "%spx" % height
-            
+           
+        # Create an Iframe (BTW, HTML() is fantastic! 
         HtmlString += """
-            <iframe id = 'IframeView%s' src = "javascript: '%s' "  style = 'width:%s;height:%s;display:block;' > 
+            <iframe id = 'IframeView%s' src = '%s' style = 'width:%s;height:%s;display:block;' > 
                 Your browser does not support iframes </iframe> 
-            """ % ( my.framecntr, HTMLview, width, height)
+            """ % ( my.framecntr, URL, width, height)
+        return HTML(HtmlString)
+        
+
+    def HTMLView( my, HTMLCode, width=None, height=None, toggle = True):
+        """        HTMLCode  -> Detachable View of the Code
+        
+        ONLY DOUBLE QUOTES PERMITTED IN HTMLview -- use &apos; for single quotes"""
+        
+        # Each LaunchView is unique -- probably could have one LaunchView + arguments, 
+        # but the data to be displayed (i.e., the would-be argument) is the lion's 
+        # share of the definition
+        my.framecntr += 1
+        
+        #Javascript is weird
+        HTMLJS = HTMLCode.replace('</script>', '<\/script>' )
+        HTMLlist = HTMLJS.split('\n')
+        
+        #Split into lines for the win_doc.writeln commands
+        HTMLtext = "' '"
+        for line in HTMLlist:
+            HTMLtext += ", '%s' " % line
+ 
+        HtmlString  = ""
+        
+        # toggle = False suppresses the popup window
+        if( toggle):
+            HtmlString += """
+
+           <script type='text/javascript'> 
+                
+                function LaunchView%s( ) {
+                    var iFrame = document.getElementById('IframeView%s');
+                    var iStringArray = [ %s ];
+                    var DetachedName = 'Detached%s';
+                    if( iFrame.style.display == 'block') {
+                        var win = window.open('about:blank', DetachedName ,'height=500,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+                        
+                        var win_doc = win.document;
+                        win_doc.open();
+                        win_doc.writeln('<!DOCTYPE html><htm' + 'l><head><body>');
+                        for (var i = 0; i < iStringArray.length; i++) {
+                            win_doc.writeln( iStringArray[i] );
+                        };
+                        win_doc.writeln('</body></ht' + 'ml>');
+                        win_doc.close();
+                        iFrame.style.display = 'none'
+                    } else { 
+                        iFrame.style.display = 'block'
+                    };
+                }
+                
+            </script>  
+      
+                <input type='button' value='toggle view' onclick='LaunchView%s()'> <br>
+                
+            """ % ( my.framecntr, my.framecntr, HTMLtext, my.framecntr,my.framecntr ) 
+        
+        # Set width, height
+        if( width == None):
+            width = "95%"
+        elif( type(width) != type( "500px" )):
+            width = "%spx" % width
+            
+        if( height == None):
+            height = "24em"
+        elif( type(height) != type( "500px" )):
+            height = "%spx" % height
+           
+        HTMLJS = HTMLJS.replace('"','&quot;' )
+        
+        # Create an Iframe (BTW, HTML() is fantastic! 
+        HtmlString += """
+            <iframe id = 'IframeView%s' srcdoc = '%s'  src = "javascript: '%s' " style = 'width:%s;height:%s;display:block;' > 
+                Your browser does not support iframes </iframe> 
+            """ % ( my.framecntr, HTMLCode, HTMLJS, width, height)
         return HTML(HtmlString)
         
 FramesAndArrays = DetachableView( )
+
+Precision = 5
+ComplexUnitString = "j"
 
 try:
     cround
@@ -63,126 +147,172 @@ except:
     def cround(z,n=5): return complex(round(z.real,n), round(z.imag,n)) 
 
 
-def View( tmp ):
-    "array or dataframe ->  Detachable View in Notebook"
-    
-    try:
-        tmp.shape
-    except:
-        print( "View cannot create a display for an object %s " % type(tmp) )
-        return 
-    
-    nme = "Name(s): "
-    for nm in get_ipython().magic(u'who_ls'): 
-        if( eval(nm) is tmp ):
-            nme += nm + str(" ")
 
+def FormatForView( entry ): 
+    "entry -> nice representation for this object"
+    if( type(entry)  in  [ float, numpy.float16, numpy.float32, numpy.float64 ] ):
+        entry = round(entry,Precision)        
+    elif( type(entry) in [ complex, numpy.complex64, numpy.complex128 ] ):
+        entry = cround(entry,Precision)
+        entry = " %s + %s %s" % (entry.real, entry.imag, ComplexUnitString)
+    return entry 
+
+
+def View( DataFrameOrArray ):
+    """array or dataframe ->  Detachable View in Notebook
+    
+    If DataFrameOrArray is either a Pandas Dataframe or a Numpy array 
+    (anything which has a .shape), then View creates a custom view with
+    relevant information and places it in a detachable view.  Otherwise, 
+    View returns a detachable view of the standard representation. 
+    
+    Clicking on the [toggle view] button detaches the View and places
+    it in a popup.  Clicking again restores the original inline view. 
+    
+    NOTE:  DESIGNED TO WORK WITH PYLAB!
+    
+    Examples: 
+    
+    In [ ]: A = array( [ [1,2],[3,4] ] )
+            View(A)
+    
+    Out[ ]: [toggle view]
+            Formatted table with scrollbars if necessary
+    
+    
+    
+    In [ ]: from pandas import DataFrame
+            B = DataFrame( A , index = [1,2], columns = ["A","B"] )
+            View(B)   
+            
+    Out[ ]: [toggle view]
+            Formatted table with scrollbars if necessary
+            and with Column/Row headings
+
+
+    In [ ]: C = randn( 50, 50) # Gaussian random sampled 50x50 array
+            View(C)
+            
+    Out[ ]: [toggle view]
+            Scrolled View of Large Matrix
+    
+    """
+    
+    # Is this a data frame (based on existence of column/index options
     IsDF = False
-    try:
-        tmp.index
+    try: 
+        DataFrameOrArray[DataFrameOrArray.columns[0]][DataFrameOrArray.index[0]]
+        len( DataFrameOrArray.columns )
+        len( DataFrameOrArray.index )
         IsDF = True
     except:
         pass 
+
+    # Standard Rep if not a DataFrame or an Array
+    try:
+        DataFrameOrArray.shape
+        if( not IsDF ):
+            DataFrameOrArray.dtype.names
+    except:
+        return FramesAndArrays.HTMLView(str(DataFrameOrArray) )
     
-    if( len(tmp.shape) == 1 ):
-        if( tmp.dtype.names ):
-            nrows = len(tmp)
-            ncols = len( tmp.dtype.names ) 
+    # Find all names that instance is bound to
+    nme = "Name(s): "
+    for nm in get_ipython().magic(u'who_ls'): 
+        if( eval(nm) is DataFrameOrArray ):
+            nme += nm + str(", ")
+    nme = nme[0:-2]
+
+    
+    # Establish values for nrows and ncols
+    if( len(DataFrameOrArray.shape) == 1 ):
+        if( DataFrameOrArray.dtype.names ):
+            nrows = len(DataFrameOrArray)
+            ncols = len( DataFrameOrArray.dtype.names ) 
         else:
-            ncols = len(tmp)
+            ncols = len(DataFrameOrArray)
             nrows = 1
     else:
-        nrows, ncols = tmp.shape
+        nrows, ncols = DataFrameOrArray.shape
         
+    # Not too small, but after height = 40em, wdth = 80 em, scrollbars
     hght = "%sem" % max(  8, min( 2*nrows+8, 40 ))
     wdth = "%sem" % max( 40, min( 4*ncols+4, 80 ))
     
+    # Create header info for the 3 types -- array, Structured Array, DataFrame
     if( IsDF ):
         typ = "DataFrame: Entries via  Name[col][row] "
-        shp = ( len( tmp.index), len(tmp.columns) )
+        shp = ( len( DataFrameOrArray.index), len(DataFrameOrArray.columns) )
         dtp = ""
-        for tp in tmp.dtypes:
+        for tp in DataFrameOrArray.dtypes:
             dtp += "%s, " % tp
-    elif( tmp.dtype.names ):
+    elif( DataFrameOrArray.dtype.names ):
         typ = "Structured Array: Entries via  Name[col][row] "
-        shp = ( tmp.shape[0], len(tmp.dtype.names) )
+        shp = ( DataFrameOrArray.shape[0], len(DataFrameOrArray.dtype.names) )
         dtp = ""
-        for tp in tmp.dtype.descr:
+        for tp in DataFrameOrArray.dtype.descr:
             dtp += "%s, " % tp[1]
         dtp = dtp.replace("<","&amp;lt;")
         dtp = dtp.replace(">","&amp;gt;")
     elif( nrows == 1 ):
         typ = "Numpy 1D Array: Entries via  Name[index] "
-        shp = tmp.shape
-        dtp = tmp.dtype
+        shp = DataFrameOrArray.shape
+        dtp = DataFrameOrArray.dtype
     else:
         typ = "Numpy Array: Entries via  Name[row, col] "
-        shp = tmp.shape
-        dtp = tmp.dtype
+        shp = DataFrameOrArray.shape
+        dtp = DataFrameOrArray.dtype
     
-    HtmlString  = "<style>"
-    HtmlString += "  table   { width:95%;border:1px solid LightGray;border-spacing:0;border-collapse: collapse;margin-left: 0; }"
-    HtmlString += "  th      { border:1px solid LightGray;padding:2px 4px; } "
-    HtmlString += "  td      { border:1px solid LightGray;padding:2px 4px;text-align:center; } "
-    HtmlString += "  caption { text-align:left; } "
-    HtmlString += "  bcap   { font-size:larger; } "
-    HtmlString += "</style>"
+    # Style and Header Info
+    HtmlString   = """
+    <style>
+        table   { width:95%;border:1px solid LightGray;border-spacing:0;border-collapse: collapse; }
+        th      { border:1px solid LightGray;padding:2px 4px; white-space:nowrap; } 
+        td      { border:1px solid LightGray;padding:2px 4px;text-align:center;white-space:nowrap; } 
+        caption { text-align:left; } 
+        #bcap   { font-size:larger; } 
+    </style>
+    """
     
-    HtmlString += "<div> <b id = &quot; #bcap &quot;> %s  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | </b>" % nme
-    HtmlString += "&nbsp; &nbsp; &nbsp; &nbsp; %s  <sub> &nbsp; </sub> <br>  <table border=1 ><caption> " % typ
-    HtmlString += " Size: %s  &nbsp;&nbsp;&nbsp;&nbsp Type(s): %s   </caption>" % ( shp, dtp ) 
+    HtmlString  += """
+    <div> <b id =  "bcap" > %s  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | </b>
+            &nbsp; &nbsp; &nbsp; &nbsp; %s  <sub> &nbsp; </sub> <br>  
+        <table border=1 >
+            <caption> Size: %s  &nbsp;&nbsp;&nbsp;&nbsp; Type(s): %s   </caption>
+    """  % ( nme, typ, shp, dtp ) 
     
+    # Create HTML5 table of given structure -- lots of details, but mainly iterating over rows and
+    # columns to insert <tr>, <th>, and <td> tags as appropriate
     if( IsDF ):
         HtmlString += "<tr> <th> &nbsp; </th>"
-        for nme in tmp.columns:
+        for nme in DataFrameOrArray.columns:
             HtmlString += '<th> %s </th> ' % nme 
         HtmlString += "</tr>" 
-        for idx in tmp.index:
+        for idx in DataFrameOrArray.index:
             HtmlString += '<tr><td><b> %s </b></td> ' % idx
-            for col in tmp.columns:
-                entry = tmp[col][idx]
-                if(type(entry) in [ float, numpy.float16, numpy.float32, numpy.float64 ]  ): 
-                    entry = round(entry,5)
-                elif(type(entry)==complex):
-                    entry = cround(entry,5)
-                HtmlString += '<td> %s </td>  ' % entry
+            for col in DataFrameOrArray.columns:
+                HtmlString += '<td> %s </td> ' %  FormatForView(DataFrameOrArray[col][idx]) 
             HtmlString += "</tr>"
-    elif( tmp.dtype.names ):
+    elif( DataFrameOrArray.dtype.names ):
         HtmlString += "<tr> "
-        for nme in tmp.dtype.names:
+        for nme in DataFrameOrArray.dtype.names:
             HtmlString += '<th> %s </th> ' % nme 
         HtmlString += "</tr>  "
         for row in range(ncols):
             HtmlString += "<tr>"
-            for nme in tmp.dtype.names:
-                entry = tmp[nme][row]
-                if( type(entry)  in [ float, numpy.float16, numpy.float32, numpy.float64 ] ):
-                    entry = round(entry,5)
-                elif(type(entry)==complex):
-                    entry = cround(entry,5)
-                HtmlString += '<td> %s </td>  ' % entry
+            for nme in DataFrameOrArray.dtype.names:
+                HtmlString += '<td> %s </td> ' %  FormatForView(DataFrameOrArray[nme][row]) 
             HtmlString += "</tr>  "
     else:
         for row in range(nrows):
             HtmlString += "<tr>"
             for col in range(ncols):
-                if(len(tmp.shape) > 1 ):
-                    entry = tmp[row,col]
-                    if(type(entry)  in [ float, numpy.float16, numpy.float32, numpy.float64 ] ):
-                        entry = round(entry,5)
-                    elif(type(entry)==complex):
-                        entry = cround(entry,5)
-                    HtmlString += '<td> %s </td> ' % entry
+                if(len(DataFrameOrArray.shape) > 1 ):
+                    HtmlString += '<td> %s </td> ' %  FormatForView(DataFrameOrArray[row,col])
                 else:
-                    entry = tmp[col]
-                    if( type(entry) in [ float, numpy.float16, numpy.float32, numpy.float64  ] ):
-                        entry = round(entry,5)
-                    elif(type(entry)==complex):
-                        entry = cround(entry,5)
-                    HtmlString += '<td> %s </td> ' % entry
+                    HtmlString += '<td> %s </td> ' %  FormatForView(DataFrameOrArray[col])
             HtmlString += "</tr>  "
     
     HtmlString += "  </table></div> " 
     return  FramesAndArrays.HTMLView(HtmlString, width = wdth, height=hght)
-            
-
+ 
