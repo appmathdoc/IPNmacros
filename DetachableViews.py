99352aa1ebe84e86de4b86e8e3da85f09e0deb7b
Jeff Knisley <knisleyj@etsu.edu>
2013-07-03 10:29:48 -0400

    FirstDraft of Final version of DetchableViews.py

--- DetachableViews.py
+++ DetachableViews.py
@@ -1,47 +1,76 @@
-## Doesn't yet work in Firefox for every possible argument (waiting for srcdoc -- Probably Aug 2013 in Firefox 23) 
+## Doesn't yet work in Firefox for every possible argument (waiting for srcdoc -- Probably Oct 2013 in Firefox 25) 
 
 ## Tested on Chrome 28, Firefox 22, and Safari 5.1.7 on Windows 7 Home, Professional
 
+from __future__ import division
 from IPython.display import HTML
 
 class DetachableView:
     "ONLY DOUBLE QUOTES PERMITTED IN HTMLview -- use &apos; for single quotes"
     def __init__(my, PostType = "OneWay" ): #PostType is for later
         my.framecntr = 0 
+        lttrs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+        
+        my.frameSig = ""
+        for i in range(20):
+            my.frameSig += lttrs[ randint(len(lttrs)) ]
 
-    def URLView( my, URL, toggle = True, width=None, height=None):
+    def URLView( my, URL, width=None, height=None, toggle = True,  message = None):
         """ URLView(URL) -> Detachable View of the page at URL
         
-            ONLY DOUBLE QUOTES PERMITTED IN HTMLview -- use &apos; for single quotes"""
+            if message != None, then postMessage API used to send message to Views"""
         
         # Each LaunchView is unique -- probably could have one LaunchView + arguments, 
         # but the data to be displayed (i.e., the would-be argument) is the lion's 
         # share of the definition
         my.framecntr += 1
+        fcntr = my.frameSig + str(my.framecntr)
         
         HtmlString = ""
         # toggle = False suppresses the popup window
         if( toggle):
             HtmlString += """
 
-           <script type='text/javascript'> 
-                
-                function LaunchView%s( ) {
-                    var iFrame = document.getElementById('IframeView%s');
-                    if( iFrame.style.display == 'block') {
-                        var win = window.open( '%s', 'Detached%s' ,'height=500,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
-                        
+               <script type='text/javascript'>
+                    var win;
+                    var messageHandler%s ;
+                    var MessageRepeater%s ;
+                    function LaunchView%s( ) {
+                        var iFrame = document.getElementById('IframeView%s');
+                        if( iFrame.style.display == 'block') {
+                            win = window.open( '%s', 'Detached%s' ,'height=500,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+                    """ % (fcntr, fcntr, fcntr, fcntr, URL, fcntr ) 
+            if( message != None):
+                HtmlString += """
+                            MessageRepeater%s = setInterval( function() {
+                                win.postMessage('%s', '*' ) ;
+                            },100);
+        
+                             messageHandler%s = function(event){
+                                if( event.data == 'done'  ) {
+                                    clearInterval( MessageRepeater%s );
+                                    window.removeEventListener('message',arguments.callee,false)
+                                }
+                            }
+                            window.addEventListener('message',messageHandler%s, false);
+                  """ %(fcntr, message, fcntr, fcntr, fcntr)
+
+            HtmlString += """
                         iFrame.style.display = 'none'
                     } else { 
                         iFrame.style.display = 'block'
                     };
+
                 }
                 
-            </script>  
+                </script>  
       
                 <input type='button' value='toggle view' onclick='LaunchView%s()'> <br>
                 
-            """ % ( my.framecntr, my.framecntr, URL, my.framecntr, my.framecntr ) 
+            """ % fcntr 
         
         # Set width, height
         if( width == None):
@@ -58,7 +87,24 @@ class DetachableView:
         HtmlString += """
             <iframe id = 'IframeView%s' src = '%s' style = 'width:%s;height:%s;display:block;' > 
                 Your browser does not support iframes </iframe> 
-            """ % ( my.framecntr, URL, width, height)
+            """ % ( fcntr, URL, width, height)
+
+        if( message != None):
+            HtmlString += """
+                <script type="text/javascript">
+
+                    var MessageHandler%s = function(event){
+                        if( event.data == 'ready' ) {
+                            document.getElementById('IframeView%s').contentWindow.postMessage('%s', '*' ) ;
+                        }
+                        if( event.data == 'done'  ) {
+                            window.removeEventListener('message',arguments.callee,false)
+                        }
+                    }
+            
+                    window.addEventListener('message',MessageHandler%s, false);
+            
+                </script> """ %(fcntr, fcntr, message, fcntr)
         return HTML(HtmlString)
         
 
@@ -71,6 +117,7 @@ class DetachableView:
         # but the data to be displayed (i.e., the would-be argument) is the lion's 
         # share of the definition
         my.framecntr += 1
+        fcntr = my.frameSig + str(my.framecntr)
         
         #Javascript is weird
         HTMLJS = HTMLCode.replace('</script>', '<\/script>' )
@@ -114,7 +161,7 @@ class DetachableView:
       
                 <input type='button' value='toggle view' onclick='LaunchView%s()'> <br>
                 
-            """ % ( my.framecntr, my.framecntr, HTMLtext, my.framecntr,my.framecntr ) 
+            """ % ( fcntr, fcntr, HTMLtext, fcntr, fcntr ) 
         
         # Set width, height
         if( width == None):
@@ -133,9 +180,11 @@ class DetachableView:
         HtmlString += """
             <iframe id = 'IframeView%s' srcdoc = '%s'  src = "javascript: '%s' " style = 'width:%s;height:%s;display:block;' > 
                 Your browser does not support iframes </iframe> 
-            """ % ( my.framecntr, HTMLCode, HTMLJS, width, height)
+            """ % ( fcntr, HTMLCode, HTMLJS, width, height)
         return HTML(HtmlString)
-        
+
+## View -- for tables, etc. 
+
 FramesAndArrays = DetachableView( )
 
 Precision = 5
@@ -333,6 +382,172 @@ def View( DataFrameOrArray ):
     
     HtmlString += "  </table></div> " 
     return  FramesAndArrays.HTMLView(HtmlString, width = wdth, height=hght)
+    
+## ViewDecisionTree  for DecisionTree viewer
+
+import numpy as np
+
+from sklearn import tree
+
+from sklearn.tree import _tree 
+
+
+def export_json(decision_tree, out_file=None, feature_names=None):
+    """Export a decision tree in JSON format.
+
+    This function generates a JSON representation of the decision tree,
+    which is then written into `out_file`. Once exported, graphical renderings
+    can be generated using, for example::
+
+        $ dot -Tps tree.dot -o tree.ps      (PostScript format)
+        $ dot -Tpng tree.dot -o tree.png    (PNG format)
+
+    Parameters
+    ----------
+    decision_tree : decision tree classifier
+        The decision tree to be exported to JSON.
+
+    out : file object or string, optional (default=None)
+        Handle or name of the output file.
+
+    feature_names : list of strings, optional (default=None)
+        Names of each of the features.
+
+    Returns
+    -------
+    out_file : file object
+        The file object to which the tree was exported.  The user is
+        expected to `close()` this object when done with it.
+
+    Examples
+    --------
+    >>> from sklearn.datasets import load_iris
+    >>> from sklearn import tree
+
+    >>> clf = tree.DecisionTreeClassifier()
+    >>> iris = load_iris()
+
+    >>> clf = clf.fit(iris.data, iris.target)
+    >>> import tempfile
+    >>> out_file = tree.export_json(clf, out_file=tempfile.TemporaryFile())
+    >>> out_file.close()
+    """
+#   import numpy as np
+
+#    from sklearn.tree import _tree
+
+    def arr_to_py(arr):
+        arr = arr.ravel()
+        wrapper = float
+        if np.issubdtype(arr.dtype, np.int):
+            wrapper = int
+        return map(wrapper, arr.tolist())
+
+
+    def node_to_str(tree, node_id):
+        node_repr = '"error": %.4f, "samples": %d, "value": %s' \
+                    % (tree.init_error[node_id],
+                       tree.n_samples[node_id],
+                       arr_to_py(tree.value[node_id]))
+        if tree.children_left[node_id] != _tree.TREE_LEAF:
+            if feature_names is not None:
+                feature = feature_names[tree.feature[node_id]]
+            else:
+                feature = "X[%s]" % tree.feature[node_id]
+
+            label = '"label": "%s <= %.2f"' % (feature,
+                                               tree.threshold[node_id])
+            node_type = '"type": "split"'
+        else:
+            node_type = '"type": "leaf"'
+            label = '"label": "Leaf - %d"' % node_id
+        node_repr = ", ".join((node_repr, label, node_type))
+        return node_repr
+
+    def recurse(tree, node_id, parent=None):
+        if node_id == _tree.TREE_LEAF:
+            raise ValueError("Invalid node_id %s" % _tree.TREE_LEAF)
+
+        left_child = tree.children_left[node_id]
+        right_child = tree.children_right[node_id]
+
+        # Open node with description
+        out_file.write('{%s' % node_to_str(tree, node_id))
+
+        # write children
+        if left_child != _tree.TREE_LEAF:  # and right_child != _tree.TREE_LEAF
+            out_file.write(', "children": [')
+            recurse(tree, left_child, node_id)
+            out_file.write(', ')
+            recurse(tree, right_child, node_id)
+            out_file.write(']')
+
+        # close node
+        out_file.write('}')
+
+    if out_file is None:
+        out_file = open("tree.json", "w")
+    elif isinstance(out_file, basestring):
+        out_file = open(out_file, "w")
+
+    if isinstance(decision_tree, _tree.Tree):
+        recurse(decision_tree, 0)
+    else:
+        recurse(decision_tree.tree_, 0)
+
+    return out_file
+
+import StringIO
+
+def ViewDecisionTree( decision_tree, feature_names=None):
+    """ViewDecisionTree( decision_tree ) ->  DetachableView of the tree
+
+    This function generates a JSON representation of the decision tree,
+    which is displayed as a Detachable View in the IPython Notebook.
+
+    Parameters
+    ----------
+    decision_tree : decision tree classifier
+        The decision tree to be exported to JSON.
+
+    feature_names : list of strings, optional (default=None)
+        Names of each of the features.
+
+    Returns
+    -------
+    An inline view (for HTML5 compliant browsers) along with a button that 
+    launches a detached view (in all browsers)
+
+    Examples
+    --------
+    In [ ]: from sklearn.datasets import load_iris
+            from sklearn import tree
+            clf = tree.DecisionTreeClassifier()
+            iris = load_iris()
+
+            clf = clf.fit(iris.data, iris.target)
+            ViewDecisionTree(clf) #Only in IPython Notebook
+            
+    Out[ ]: [toggle view]
+            Detachable View of Tree
+
+    """
+
+
+    output = StringIO.StringIO()
+    
+    out_file = export_json(clf, out_file=output)
+    msg = out_file.getvalue()
+    out_file.close()
+    TheHTML = HTMLforD3JSdecisiontree % msg
+    return D3JSViewer.HTMLView( TheHTML, width = "100%", height="500px")    
+
+
+    
+
+
+
 
 HTML(""" The <b>DetachableView</b> class is ready <br>
-         The <b>View</b> function is ready """ )
+         The <b>View</b> function is ready <br>
+         The <b>ViewDecisionTree</b> function is ready""" )
