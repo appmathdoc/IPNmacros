4508f1f77829125e23b09520add2dd598b623f08
Jeff Knisley <knisleyj@etsu.edu>
2013-08-31 17:44:26 -0400

    Changed example username

--- Lecture2.py
+++ Lecture2.py
@@ -566,7 +566,7 @@ def SubeconomyProblem(Username):
         
     return Data, [ HouseholdNames[i] for i in SubEconomy]
 
-FstEx, FstExkey = SubeconomyProblem('firstexample')
+FstEx, FstExkey = SubeconomyProblem('Lecture2')
 SmallTownData = DataFrame( FstEx, index = HouseholdNames, columns = Businesses)
 
 try:
